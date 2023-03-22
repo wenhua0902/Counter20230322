97234c072ab69ba1e242c546b9acbbd1f16aaf4d
黃汶樺 <s1100396@gm.pu.edu.tw>
2023-03-22 11:06:26 +0800

    加入樂透功能

--- app/src/main/java/tw/edu/pu/gm/s1100396/counter/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/gm/s1100396/counter/MainActivity.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.gm.s1100396.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var txv: TextView = findViewById(R.id.txv)
         txv.text = "0"
+
+        fun happy(v: View){
+            var txv: TextView = findViewById(R.id.txv)
+            txv.text = (1..100).random().toString()
+
+        }
     }
 }
