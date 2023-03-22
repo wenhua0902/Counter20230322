278d6cbd26b9f9366b612b733260b90c77cf64dd
黃汶樺 <s1100396@gm.pu.edu.tw>
2023-03-22 09:59:55 +0800

    第一次Commit

---
+++ app/src/main/java/tw/edu/pu/gm/s1100396/counter/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.gm.s1100396.counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
